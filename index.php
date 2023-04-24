db85d927aed42d1bc8e523761b6f30717879b213
sathish19961412 <sathishfreelanc5@gmail.com>
2023-04-24 10:50:25 +0530

    Basic Echo print the output

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Basic Php</title>
+</head>
+
+<body>
+<?php
+   echo "<h3>Welcome To Php Basic and Advance Tutorial in Sathish</h3>";
+?>
+</body>
+
+</html>
